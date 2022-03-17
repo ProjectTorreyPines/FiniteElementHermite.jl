4d89ef2a6edfe4dcd360724130cc27a934fe7a2b
Brendan C. Lyons <bclyons12@gmail.com>
2022-03-17 15:57:55 -0700

    Create separate repository for finite-element implementation

--- src/FiniteElementHermite.jl
+++ src/FiniteElementHermite.jl
@@ -1,5 +1,12 @@
+__precompile__()
+
 module FiniteElementHermite
 
-# Write your package code here.
+using QuadGK
+
+include("hermite.jl")
+export FE_rep, FE, D, I
+export νe, D_νe, I_νe, νo, D_νo, I_νo
+export inner_product
 
 end
