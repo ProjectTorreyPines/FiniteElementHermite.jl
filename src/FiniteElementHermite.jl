791c25f8ca8796257e0a7306c14f89c95529c0bf
Brendan C. Lyons <bclyons12@gmail.com>
2022-07-06 14:55:28 -0700

    Add computation of mass matrix, as may be needed for many applications

--- src/FiniteElementHermite.jl
+++ src/FiniteElementHermite.jl
@@ -3,10 +3,14 @@ __precompile__()
 module FiniteElementHermite
 
 using QuadGK
+using BandedMatrices
 
 include("hermite.jl")
 export FE_rep, FE, D, I
 export νe, D_νe, I_νe, νo, D_νo, I_νo
 export inner_product
+
+include("matrices.jl")
+export mass_matrix
 
 end
